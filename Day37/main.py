ca0473249b614d6edc8cb1bdd128e819919f10e9
semerdzhievradostin <radostinsemerdzhiev98@gmail.com>
2023-04-10 19:18:54 +0300

    Day37 - Habit Tracking Project: API Post Requests & Headers

---
+++ Day37/main.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+import requests
+
+
+#https://pixe.la/
+TOKEN = "TYPE at least 8 symbols for token"
+USER = "YOURUSERNAME"
+GRAPH_ID = "graph ID"
+
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": TOKEN,
+    "username": USER,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+graph_params = {
+    "id": "giti1",
+    "name": "GitLike",
+    "unit": "commit",
+    "type": "int",
+    "color": "shibafu",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+#graph_endpoint = f"{pixela_endpoint}/{USER}/graphs"
+#gr = requests.post(graph_endpoint, json=graph_params, headers=headers)
+#print(gr.text)
+
+
+today = datetime.now()
+commits = input("How many commits today?\n")
+graph_add = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": commits,
+}
+
+graph_post = f"{pixela_endpoint}/{USER}/graphs/{GRAPH_ID}"
+gr_add = requests.post(graph_post, json=graph_add, headers=headers)
+print(gr_add.text)
+
+
+#https://pixe.la/
